dc6821fe4479a26f3392caa3cd48815b17c2d147
SteveNascimento <stevecraken@gmail.com>
2021-11-19 14:38:14 -0300

    layout base

---
+++ frontend/plaforedu/craco.config.js
@@ -0,0 +1,18 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { 
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
