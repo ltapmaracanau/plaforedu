dd4c8600c2e1967637b14132ee2509f5a74eb064
SteveNascimento <stevecraken@gmail.com>
2023-01-25 16:51:27 -0300

    Log de buscas completo com filtragem de dados

--- src/components/user-settings/ListSearchLogs.jsx
+++ src/components/user-settings/ListSearchLogs.jsx
@@ -1,36 +1,255 @@
-import { Card, Empty, Layout, Table } from "antd";
+import {
+  Button,
+  Card,
+  DatePicker,
+  Empty,
+  Input,
+  Layout,
+  Space,
+  Table,
+} from "antd";
 import { useStoreActions, useStoreState } from "easy-peasy";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+import "moment/locale/pt-br";
+import locale from "antd/es/locale/pt_BR";
+
+import { SearchOutlined, FilterFilled } from "@ant-design/icons";
 
 const { Content } = Layout;
 
 export default function ListSearchLogs() {
   const logs = useStoreState((state) => state.adm.searchLogs);
   const loading = useStoreState((state) => state.adm.loadingLogs);
+  const count = useStoreState((state) => state.adm.countLogs);
   const getSearchLogs = useStoreActions((actions) => actions.adm.getSearchLogs);
 
+  const [page, setPage] = useState(1);
+
+  const [stringSearch, setStringSearch] = useState({
+    description: "",
+    user: "",
+    initialDate: "",
+    finalDate: "",
+  });
+
+  const [stringSearchMemo, setStringSearchMemo] = useState({
+    description: "",
+    user: "",
+    initialDate: "",
+    finalDate: "",
+  });
+
+  const [activeColumsFilter, setActiveColumsFilter] = useState({
+    description: false,
+    user: false,
+    initialDate: false,
+    finalDate: false,
+  });
+
   useEffect(() => {
-    getSearchLogs();
+    getSearchLogs({ page: 1 });
   }, [getSearchLogs]);
 
+  const handleSearch = (dataIndex) => {
+    setActiveColumsFilter((antig) => ({
+      ...antig,
+      [`${dataIndex}`]: true,
+    }));
+    const newSearch = {
+      ...stringSearchMemo,
+      [`${dataIndex}`]: stringSearch[`${dataIndex}`],
+    };
+    setStringSearchMemo(newSearch);
+    setPage(1);
+    getSearchLogs({
+      page: 1,
+      ...newSearch,
+    });
+  };
+
+  const handleReset = (dataIndex) => {
+    setActiveColumsFilter((antig) => ({
+      ...antig,
+      [`${dataIndex}`]: false,
+    }));
+    const newSearch = {
+      ...stringSearchMemo,
+      [`${dataIndex}`]: "",
+    };
+    setStringSearchMemo(newSearch);
+    setStringSearch(newSearch);
+    setPage(1);
+    getSearchLogs({
+      page: 1,
+      ...newSearch,
+    });
+  };
+
+  const getColumnSearchProps = (dataIndex, name) => ({
+    filterDropdown: () => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          placeholder={`Buscar ${name}`}
+          value={stringSearch[`${dataIndex}`]}
+          onChange={(e) => {
+            setStringSearch((antig) => ({
+              ...antig,
+              [`${dataIndex}`]: e.target.value,
+            }));
+          }}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => {
+              handleSearch(dataIndex);
+            }}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => {
+              handleReset(dataIndex);
+            }}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: () => (
+      <SearchOutlined
+        style={{
+          color: activeColumsFilter[`${dataIndex}`] ? "#1890ff" : undefined,
+        }}
+      />
+    ),
+  });
+
+  const getDateColumsSearchParams = () => ({
+    filterDropdown: () => (
+      <div
+        style={{
+          padding: 8,
+          display: "flex",
+          flexDirection: "column",
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Space
+          direction="horizontal"
+          style={{
+            marginBottom: "5px",
+          }}
+        >
+          <DatePicker
+            value={stringSearch.initialDate}
+            locale={locale}
+            format={"DD/MM/YYYY"}
+            onChange={(e) => {
+              setStringSearch((antg) => ({
+                ...antg,
+                initialDate: e,
+              }));
+            }}
+            placeholder={"Data Inicial"}
+          />
+          <DatePicker
+            value={stringSearch.finalDate}
+            format={"DD/MM/YYYY"}
+            locale={locale}
+            onChange={(e) => {
+              setStringSearch((antg) => ({
+                ...antg,
+                finalDate: e,
+              }));
+            }}
+            placeholder={"Data Final"}
+          />
+        </Space>
+        <Space direction="horizontal">
+          <Button
+            type="primary"
+            onClick={() => {
+              handleSearch("initialDate");
+              handleSearch("finalDate");
+            }}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => {
+              handleReset("initialDate");
+              handleReset("finalDate");
+            }}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: () => (
+      <FilterFilled
+        style={{
+          color:
+            activeColumsFilter.initialDate || activeColumsFilter.finalDate
+              ? "#1890ff"
+              : undefined,
+        }}
+      />
+    ),
+  });
+
   const columns = [
+    {
+      title: "Busca",
+      dataIndex: "description",
+      key: "description",
+      width: "50%",
+      ...getColumnSearchProps("description", "palavra"),
+    },
     {
       title: "Usuário",
       dataIndex: "user",
       key: "user",
-    },
-    {
-      title: "Busca",
-      dataIndex: "string",
-      key: "string",
+      ...getColumnSearchProps("user", "usuário"),
     },
     {
       title: "Dia/Hora",
-      dataIndex: "datetime",
-      key: "datetime",
+      dataIndex: "createdAt",
+      key: "createdAt",
       render: (item) => {
         return <>{new Date(item).toLocaleString()}</>;
       },
+      ...getDateColumsSearchParams(),
     },
   ];
 
@@ -45,9 +264,30 @@ export default function ListSearchLogs() {
     >
       <Content style={{ width: "100%" }}>
         <Table
-          dataSource={logs}
+          dataSource={logs.map((item) => ({
+            user: item.user.name || "Anônimo",
+            description: item.description,
+            createdAt: item.createdAt,
+            id: item.id,
+          }))}
           loading={loading}
           rowKey={"id"}
+          pagination={{
+            onChange: (page) => {
+              setPage(page);
+              getSearchLogs({
+                page: page,
+                ...stringSearchMemo,
+              });
+            },
+            pageSize: 20,
+            total: count,
+            showSizeChanger: false,
+            current: page,
+            defaultCurrent: 1,
+            hideOnSinglePage: false,
+          }}
+          bordered
           locale={{
             emptyText: (
               <Card>
