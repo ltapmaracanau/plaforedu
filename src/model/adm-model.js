3149ac744efb1057ad9f943133a73c101ef89c10
SteveNascimento <stevenascimentodeveloper@gmail.com>
2024-03-28 15:05:36 -0300

    refact: Removendo console.log

--- src/model/adm-model.js
+++ src/model/adm-model.js
@@ -110,7 +110,6 @@ const admModel = {
     return await services.admService
       .getRandomTrailsHomepage()
       .then((response) => {
-        console.log(response);
         if (response.status === 200) {
           actions.setRandomTrails(response.data.formativeTrails);
         }
