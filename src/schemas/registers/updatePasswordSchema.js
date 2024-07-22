c934edb2089d16e6b8cf932d81d13016d72c6f1f
SteveNascimento <stevenascimentodeveloper@gmail.com>
2024-07-22 18:42:59 -0300

    refact: Tratativa senha igual à antiga

--- src/schemas/registers/updatePasswordSchema.js
+++ src/schemas/registers/updatePasswordSchema.js
@@ -8,6 +8,10 @@ export const updatePasswordSchema = yup.object().shape({
     .matches(
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
       "Precisa ter: uma letra maiúscula; uma letra minúscula; um número; um caractere especial; no mínimo 8 caracteres"
+    )
+    .notOneOf(
+      [yup.ref("oldPassword"), null],
+      "A nova senha não pode ser igual a senha antiga!"
     ),
   password2: yup
     .string()
